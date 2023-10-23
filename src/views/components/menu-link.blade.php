e14e68ac8df24058a220efc60e7ad9f266cb45a9
Romain Chérot <romain.cherot@free.fr>
2023-10-23 17:53:00 +0200

    Fix submenu enclosed in link

--- src/views/components/menu-link.blade.php
+++ src/views/components/menu-link.blade.php
@@ -1,9 +1,9 @@
 <li class="{{ $classes }}">
     <a href="{{ $href }}" class="{{ $active ? 'is-active' : '' }}">
-        @if($has_icon)
-        @svg($icon)
+        @if ($has_icon)
+            @svg($icon)
         @endif
         {!! $title !!}
-        {!! $submenu_html !!}
     </a>
+    {!! $submenu_html !!}
 </li>
